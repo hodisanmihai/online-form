6e57400f424152a44a9432f61a738a345afbef97
hodisan mihai <mihaihodisan@gmail.com>
2024-02-10 23:13:14 +0200

    uodaer

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('yesBtn').addEventListener('click', function() {
+    alert('That\'s wonderful! Happy Valentine\'s Day!');
+  });
+  
+  document.getElementById('noBtn').addEventListener('click', function() {
+    alert('Oh no! Maybe next time!');
+  });
+  
